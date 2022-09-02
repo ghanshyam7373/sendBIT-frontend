dd243dfb4a56a7fe113fe99ee6ae6450c63b6f8c
aman162000 <borseaman16@gmail.com>
2022-09-02 18:49:18 +0530

    Fix theme.js bugs

--- scripts/themes.js
+++ scripts/themes.js
@@ -1,18 +1,35 @@
-let body = document.querySelector('body');
+(()=>{
 
-document.getElementById('modecheck').addEventListener('click',()=>{
-  if(body.style.backgroundColor ==="black"){
-    body.style.backgroundColor = "white";
-    document.getElementById('navlogo').src = "/assets/dark-logo.png";
-    document.getElementById('server-name').style.color = "black";
-    document.getElementById('infoicon').style.color = "black";
-    document.getElementById('modeicon').style.color = "black";
+
+let themeBtn = document.getElementById("modecheck");
+let userPreferedScheme = window.matchMedia("(prefers-color-scheme:dark)");
+let currentTheme = localStorage.getItem("theme");
+
+if (currentTheme == "dark") {
+  document.body.classList.toggle("dark-theme");
+  document.getElementById("navlogo").src = "/assets/light-logo.png";
+} else if (currentTheme == "light") {
+  document.body.classList.toggle("light-theme");
+  document.getElementById("navlogo").src = "/assets/dark-logo.png";
+}
+
+themeBtn.addEventListener("click", () => {
+  if (userPreferedScheme.matches) {
+    document.body.classList.toggle("light-theme");
+
+
+    var theme = document.body.classList.contains("light-theme")
+      ? "light"
+      : "dark";
+  } else {
+    document.body.classList.toggle("dark-theme");
+
+    var theme = document.body.classList.contains("dark-theme")
+      ? "dark"
+      : "light";
   }
-  else{
-    body.style.backgroundColor = "black";
-    document.getElementById('navlogo').src = "/assets/light-logo.png";
-    document.getElementById('server-name').style.color = "white";
-    document.getElementById('infoicon').style.color = "white";
-    document.getElementById('modeicon').style.color = "white";
-  }
-})
+  document.getElementById("navlogo").src = theme == 'dark' ? '/assets/light-logo.png':'/assets/dark-logo.png' 
+  localStorage.setItem("theme", theme);
+});
+})();
+
